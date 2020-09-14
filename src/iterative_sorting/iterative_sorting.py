1c835df00b9e3eb9ec04a3ea67096ecac38a75f6
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-09-14 16:35:12 -0400

    finished module one

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,44 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+    #traverse through all array elements 
+    for i in range(len(arr)):
+        #find the minimum element in remaining unsorted array 
+        min_idx = i 
+        #for elment in range element-i + 1 through length of array: 
+        for j in range(i+1, len(arr)):
+            #if the array minimum index is greater than the arrays current element 
+            if arr[min_idx] > arr[j]:
+                # set the new minimum equal to this element 
+                min_idx = j 
+                #otherwise swap positions 
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    n = len(arr)
+    #traverse through all array elements 
+    for i in range(n-1): 
+        ##range(n) also work but out loop will repeat one time more than needed 
+        
+        # last i elements are already in place 
+        for j in range(0, n-i-1):
+
+            #traverse the array from 0 to n-i-1 
+            #swap if the element found is greater 
+            #than the next element 
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 
 
     return arr
+
+arr = [64, 34, 25, 12, 22, 11, 90] 
+  
+print(bubble_sort(arr))
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -40,7 +58,31 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
+    # max <- find largest element in array 
+    #initialize count array with all zeros 
+    #for j <- 0 to size 
+        #find the total count of each unique element and 
+        #store the count at jth index in count array 
+    #for i <- 1 to max 
+        #find the cumulative sum and store it in the count array itself
+    #for j <- size down to 1 
+        #restore the elemtns to array 
+        #decrease count of each element restored by 1
+    max = maximum+ 1
+    count = [0] * max 
+    for j in arr:
+        #find total count of each unique element 
+        count[j] =+1 
+    #rewrite my answer starting with 0 
+    index = 0 
 
-
+    #create a list from 0 range max so it has all 
+    #the numbers that could possibly be in that list 
+    for num in range(max):
+        #take num_in in range of zero's array (that is indexed by the numbers in max) 
+        for num_in in range(count[num]): 
+            # add the number to each index accordingly 
+            arr[index] += num
+            #increase index size each time 
+            index += 1
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)): 
+        if arr[i] == target: 
+            return i 
+        
     return -1   # not found
 
 
-# Write an iterative implementation of Binary Search
+#Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+    # find the midpoint element 
+    # length // 2
+    left = 0
+    right = len(arr) - 1
+    # keep iterating so long as left <= right
+    while left <= right:
+        midpoint = (right + left) // 2
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] > target:
+            # toss out the left side of the array 
+            # toss out the midpoint element itself as well 
+            right = midpoint - 1
+        else:
+            left = midpoint + 1
+    # we didn't find what we're looking for 
+    return -1
